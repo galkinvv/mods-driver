f06e91a83e5620612e041a3d7c13a9847a47fee7
Vasily Galkin <galkin-vv@ya.ru>
2019-10-06 18:31:31 +0300

    Modify instal_moduile.sh so it can be used without parameters.
    The parameterless behaviour is "compile and load until reboot".

--- README.md
+++ README.md
@@ -1,8 +1,6 @@
-NVIDIA MODS kernel driver
-=========================
+# NVIDIA MODS kernel driver
 
-Introduction
-------------
+## Introduction
 
 The NVIDIA MODS kernel driver is a simple kernel module which provides
 access to devices on the PCI bus for user mode programs.  It is so named
@@ -14,5 +12,56 @@ the code is in something of an unfinished form.  It is released in the hopes
 that it will save work for people who might otherwise need to implement such
 a thing.
 
-This software is not published as an official NVIDIA product.  NVIDIA will
-provide no support for this source release
+This software is licensed under GNU GPL2 and not published as an official NVIDIA product.
+NVIDIA will provide no support for this source release.
+
+## Usage
+
+### Install prerequisites
+Use your distribution package meneger to install:
+
+* Toolchain and make program required for building C kernel modules (for ubuntu it is `build-essential` package).
+* Kernel headers of your kernel version (typically is is package named like `linux*headers*`).
+
+### Load module
+Run `./install_module.sh` without parameters.
+On success it will build the module if it is not already build and load it into kernel.
+Running without parameters the script will not install module in system location and will not modify udev rules files.
+*Running with any parameters is not tested and can modify your system! See source code*
+Here is example of expected output with examples of a typical output for correct operation:
+```
+[root@pool mods-driver]# ./install_module.sh
+make: Entering directory '/opt/mods-driver/build-4.19.47-1-lts'
+make -C /lib/modules/4.19.47-1-lts/build M=/opt/mods-driver/build-4.19.47-1-lts clean
+make[1]: Entering directory '/usr/lib/modules/4.19.47-1-lts/build'
+make[1]: Leaving directory '/usr/lib/modules/4.19.47-1-lts/build'
+make: Leaving directory '/opt/mods-driver/build-4.19.47-1-lts'
+make: Entering directory '/opt/mods-driver/build-4.19.47-1-lts'
+make -C /lib/modules/4.19.47-1-lts/build M=/opt/mods-driver/build-4.19.47-1-lts modules
+make[1]: Entering directory '/usr/lib/modules/4.19.47-1-lts/build'
+  CC [M]  /opt/mods-driver/build-4.19.47-1-lts/mods_krnl.o
+  CC [M]  /opt/mods-driver/build-4.19.47-1-lts/mods_mem.o
+  CC [M]  /opt/mods-driver/build-4.19.47-1-lts/mods_irq.o
+  CC [M]  /opt/mods-driver/build-4.19.47-1-lts/mods_pci.o
+  CC [M]  /opt/mods-driver/build-4.19.47-1-lts/mods_acpi.o
+  CC [M]  /opt/mods-driver/build-4.19.47-1-lts/mods_debugfs.o
+  LD [M]  /opt/mods-driver/build-4.19.47-1-lts/mods.o
+  Building modules, stage 2.
+  MODPOST 1 modules
+  CC      /opt/mods-driver/build-4.19.47-1-lts/mods.mod.o
+  LD [M]  /opt/mods-driver/build-4.19.47-1-lts/mods.ko
+make[1]: Leaving directory '/usr/lib/modules/4.19.47-1-lts/build'
+make: Leaving directory '/opt/mods-driver/build-4.19.47-1-lts'
+Loaded module mods
+[root@pool mods-driver]# ./install_module.sh
+Already loaded module mods
+[root@pool mods-driver]# rmmod mods
+[root@pool mods-driver]# ./install_module.sh
+Loaded already compiled module mods
+[root@pool mods-driver]# ./install_module.sh
+Already loaded module mods
+[root@pool mods-driver]#
+
+```
+
+### Now you can use the software that needs this kernel module (until reboot).
--- install_module.sh
+++ install_module.sh
@@ -18,7 +18,8 @@
 # * <http://www.gnu.org/licenses/>.
 
 MODULE_NAME="mods"
-MODULE_DIR="driver/"
+KERN_VERSION=`uname -r`
+MODULE_DIR="build-${KERN_VERSION}/"
 INSMOD="/sbin/insmod"
 RMMOD="/sbin/rmmod"
 MODPROBE="/sbin/modprobe"
@@ -47,9 +48,6 @@ isloaded() {
     ( test -f "$MODULELIST" && grep -q "^$1\>" "$MODULELIST" ) || lsmod | grep -q "^$1\>"
 }
 
-# Check kernel version
-uname -r | grep -q "^[12]\.[0-5]" && die "MODS supports only kernel 2.6 or newer, your kernel version is `uname -r`"
-
 # Check arguments
 OPTION=""
 case "$1" in
@@ -62,12 +60,16 @@ case "$1" in
 --insert)
     OPTION="insert" ;;
 esac
-if [ $# -ne 1 ] || [ -z "$OPTION" ]; then
+if [ -z "$1" ]; then
+    OPTION="insert"
+fi
+if [ $# -gt 1 ] || [ -z "$OPTION" ]; then
     echo "This is installation script for MODS kernel driver"
     echo
-    echo "Usage: `basename $0` OPTION"
+    echo "Usage: `basename $0` [OPTION]"
     echo
     echo "OPTION is one of:"
+    echo "NO ARGS, --insert Compiles driver and loads it until reboot."
     echo "  -i, --install   Installs the driver in the system and loads it."
     echo "  -u, --uninstall Unloads the driver and removes it from the system."
     echo "  -r, --reload    Reloads installed driver."
@@ -88,7 +90,6 @@ if [ "$OPTION" = "uninstall" ]; then
     [ -f "/etc/modules" ] && grep -q "$MODULE_NAME" "/etc/modules" && sed -i "/$MODULE_NAME/d" "/etc/modules"
     find "/lib/modules/`uname -r`"/ -name "$MODULE_NAME.ko" -exec rm '{}' \;
     $DEPMOD -aq
-    [ -d "$MODULE_DIR" ] && rm -rf "$MODULE_DIR"
     [ -c "/dev/$MODULE_NAME" ] && rm "/dev/$MODULE_NAME"
     exit 0
 fi
@@ -106,120 +107,116 @@ if ( [ "$OPTION" = "install" ] || [ "$OPTION" = "reload" ] ) && isloaded "$MODUL
 fi
 
 # Check if module is loaded, if not, load it
-if ! isloaded "$MODULE_NAME"; then
-
-    if [ "$OPTION" != "install" ]; then
-
-        # Attempt to load the MODS driver
-        [ `id -u` -eq 0 ] && $MODPROBE -q "$MODULE_NAME" && sleep 1 && exit
-
-        # Check if there is a precompiled module and try to install it
-        if [ "$OPTION" = "insert" -a -f "${MODULE_DIR}${MODULE_NAME}.ko" ]; then
-            checkuser
-            $INSMOD "${MODULE_DIR}${MODULE_NAME}.ko" && sleep 1 && exit
-        fi
-
-        # Bail out on reload
-        [ "$OPTION" = "reload" ] && die "Unable to reload $MODULE_NAME module, please install it"
-    fi
-
-    # Check if kernel sources are available
-    [ -d "/lib/modules/`uname -r`/build" ] || die "Kernel sources are not installed.\nPlease install kernel sources using your distribution's package manager."
-
-    # Re-launch in /tmp if the current directory is not writable
-    if [ ! -w . ]; then
-        if [ ! -w /tmp ]; then
-            checkuser
-            die "/tmp is not writable for the root user"
-        fi
-        cp "$0" /tmp || exit $?
-        cp `dirname "$0"`/driver.tgz /tmp || exit $?
-        ( cd /tmp && /tmp/`basename "$0"` "$@" )
-        LASTERROR=$?
-        rm /tmp/driver.tgz /tmp/`basename "$0"`
-        exit $LASTERROR
-    fi
-
-    # Unpack module source
-    [ -d "$MODULE_DIR" ] && rm -rf "$MODULE_DIR"
-    [ -f "$DRIVER" ] || die "Driver package not found"
-    tar xzf "$DRIVER" || die "Unable to decompress kernel module sources"
-
-    # Clean the precompiled module 
-    make -C "$MODULE_DIR" clean || die "Cleanup failed"
-
-    # Compile the module 
-    make -C "$MODULE_DIR" || die "Compilation failed"
-
-    # Install the module
-    checkuser
-    if [ "$OPTION" != "insert" ]; then
-        make -C "$MODULE_DIR" install || die "Installation failed"
-        $DEPMOD -ae
-
-        # Update udev rules
-        if ! grep -R -l -q "$MODULE_NAME" "$UDEVBASEDIR"/*; then
-            RULESOK=0
-            if grep -R -l -q nvidia "$UDEVBASEDIR"/*; then
-                EXISTINGRULES=`grep -R -l nvidia "$UDEVBASEDIR"/* | grep ".rules$"`
-                EXISTINGPERM=`grep -R -l nvidia "$UDEVBASEDIR"/* | grep ".permissions$"`
-                if [ -f "$EXISTINGRULES" ]; then
-                    EXISTINGGROUP=`grep "nvidia" "$EXISTINGRULES" | sed "s/.*GROUP=\"// ; s/\".*//"`
-                    ( echo "$EXISTINGGROUP" | grep -q "=\|\"" ) || MODSRULES=`echo "$MODSRULES" | sed "s/video/$EXISTINGGROUP/"`
-                    RULESDIR=`dirname "$EXISTINGRULES"`
-                fi
-                if [ -f "$EXISTINGPERM" ]; then
-                    MODSPERM=`grep "nvidia" "$EXISTINGPERM" | sed "s/nvidia[^:]*:/mods:/"`
-                    PERMDIR=`dirname "$EXISTINGPERM"`
-                fi
-            fi
-            if [ -z "$PERMDIR" -a -z "$RULESDIR" ]; then
-                RULES=`find "$UDEVBASEDIR/" -name "*.rules" 2>/dev/null | head -n 1`
-                if [ -f "$RULES" ]; then
-                    RULESDIR=`dirname "$RULES"`
-                else
-                    RULESDIR="$UDEVBASEDIR/rules.d"
-                    [ -d "$RULESDIR" ] || mkdir "$RULESDIR"
-                fi
-            fi
-            if [ -d "$RULESDIR" ]; then
-                RULESFILE="$RULESDIR/$MODSRULESFILE"
-                echo "Writing udev rules for the $MODULE_NAME kernel module info $RULESFILE"
-                echo "$MODSRULES" > "$RULESFILE" || die "Unable to write $RULESFILE"
-                RULESOK=1
-            fi
-            if [ -d "$PERMDIR" ]; then
-                PERMFILE="$PERMDIR/$MODSPERMFILE"
-                echo "Writing udev permissions for the $MODULE_NAME kernel module info $PERMFILE"
-                echo "$MODSPERM" > "$PERMFILE" || die "Unable to write $PERMFILE"
-                RULESOK=1
-            fi
-            if [ "$RULESOK" != "1" ]; then
-                echo "Warning: Could not update udev rules!"
-                echo "Please ensure that access permissions to /dev/mods are correct"
-            fi
-        fi
-
-        # Print hint about loading the module on boot
-        if [ -f "/etc/modules" ] && [ -f "/etc/debian_version" ]; then
-            echo
-            echo "To load the $MODULE_NAME module on boot, add it to /etc/modules"
-        elif [ -f "/etc/rc.d/rc.local" ] && [ -f "/etc/redhat-release" ]; then
-            echo
-            echo "To load the $MODULE_NAME module on boot, add the following line in /etc/rc.d/rc.local:"
-            echo "modprobe mods"
-        elif [ -f "/etc/sysconfig/kernel" ] && [ -f "/etc/SuSE-release" ]; then
-            echo
-            echo "To load the $MODULE_NAME module on boot, add it to MODULES_LOADED_ON_BOOT"
-            echo "in /etc/sysconfig/kernel"
-        fi
-    fi
-
-    # Try to insert the compiled module
-    if [ "$OPTION" = "insert" ]; then
-        $INSMOD "${MODULE_DIR}${MODULE_NAME}.ko" || die "Unable to install the module"
-    else
-        $MODPROBE "${MODULE_NAME}" || die "Unable to install the module"
-    fi
-    sleep 1
+if isloaded "$MODULE_NAME"; then
+    echo "Already loaded module ${MODULE_NAME}"
+    exit 0
 fi
+
+if [ "$OPTION" != "install" ]; then
+
+    # Attempt to load the MODS driver
+    [ `id -u` -eq 0 ] && $MODPROBE -q "$MODULE_NAME" && sleep 1 && echo "Loaded preinstalled module ${MODULE_NAME}" && exit
+
+    # Check if there is a precompiled module and try to install it
+    if [ "$OPTION" = "insert" -a -f "${MODULE_DIR}${MODULE_NAME}.ko" ]; then
+        checkuser
+        $INSMOD "${MODULE_DIR}${MODULE_NAME}.ko" && sleep 1 && echo "Loaded already compiled module ${MODULE_NAME}" && exit
+    fi
+
+    # Bail out on reload
+    [ "$OPTION" = "reload" ] && die "Unable to reload $MODULE_NAME module, please install it"
+fi
+
+# Check if make is installed
+command -v make >/dev/null || die "'make' program is not installed.\nPlease install Toolchain and make program required for building C kernel modules\n(for ubuntu it is 'build-essential' package)."
+
+# Check if kernel sources are available
+[ -d "/lib/modules/${KERN_VERSION}/build" ] || die "Kernel sources are not installed.\nPlease install kernel headers for version ${KERN_VERSION} using your distribution's package manager.\nTypically is is package named like 'linux*headers*'"
+
+[ -w . ] || die "Current directory must be writable since in-tree build is used"
+
+mkdir -p "${MODULE_DIR}"
+
+(
+    cd "${MODULE_DIR}"
+    ln -fs ../Makefile ../*.c ../*.h ./
+)
+
+# Clean the precompiled module
+make -C "$MODULE_DIR" clean || die "Cleanup failed"
+
+# Compile the module
+make -C "$MODULE_DIR" || die "Compilation failed"
+
+# Install the module
+checkuser
+if [ "$OPTION" != "insert" ]; then
+    make -C "$MODULE_DIR" install || die "Installation failed"
+    $DEPMOD -ae
+
+    # Update udev rules
+    if ! grep -R -l -q "$MODULE_NAME" "$UDEVBASEDIR"/*; then
+        RULESOK=0
+        if grep -R -l -q nvidia "$UDEVBASEDIR"/*; then
+            EXISTINGRULES=`grep -R -l nvidia "$UDEVBASEDIR"/* | grep ".rules$"`
+            EXISTINGPERM=`grep -R -l nvidia "$UDEVBASEDIR"/* | grep ".permissions$"`
+            if [ -f "$EXISTINGRULES" ]; then
+                EXISTINGGROUP=`grep "nvidia" "$EXISTINGRULES" | sed "s/.*GROUP=\"// ; s/\".*//"`
+                ( echo "$EXISTINGGROUP" | grep -q "=\|\"" ) || MODSRULES=`echo "$MODSRULES" | sed "s/video/$EXISTINGGROUP/"`
+                RULESDIR=`dirname "$EXISTINGRULES"`
+            fi
+            if [ -f "$EXISTINGPERM" ]; then
+                MODSPERM=`grep "nvidia" "$EXISTINGPERM" | sed "s/nvidia[^:]*:/mods:/"`
+                PERMDIR=`dirname "$EXISTINGPERM"`
+            fi
+        fi
+        if [ -z "$PERMDIR" -a -z "$RULESDIR" ]; then
+            RULES=`find "$UDEVBASEDIR/" -name "*.rules" 2>/dev/null | head -n 1`
+            if [ -f "$RULES" ]; then
+                RULESDIR=`dirname "$RULES"`
+            else
+                RULESDIR="$UDEVBASEDIR/rules.d"
+                [ -d "$RULESDIR" ] || mkdir "$RULESDIR"
+            fi
+        fi
+        if [ -d "$RULESDIR" ]; then
+            RULESFILE="$RULESDIR/$MODSRULESFILE"
+            echo "Writing udev rules for the $MODULE_NAME kernel module info $RULESFILE"
+            echo "$MODSRULES" > "$RULESFILE" || die "Unable to write $RULESFILE"
+            RULESOK=1
+        fi
+        if [ -d "$PERMDIR" ]; then
+            PERMFILE="$PERMDIR/$MODSPERMFILE"
+            echo "Writing udev permissions for the $MODULE_NAME kernel module info $PERMFILE"
+            echo "$MODSPERM" > "$PERMFILE" || die "Unable to write $PERMFILE"
+            RULESOK=1
+        fi
+        if [ "$RULESOK" != "1" ]; then
+            echo "Warning: Could not update udev rules!"
+            echo "Please ensure that access permissions to /dev/mods are correct"
+        fi
+    fi
+
+    # Print hint about loading the module on boot
+    if [ -f "/etc/modules" ] && [ -f "/etc/debian_version" ]; then
+        echo
+        echo "To load the $MODULE_NAME module on boot, add it to /etc/modules"
+    elif [ -f "/etc/rc.d/rc.local" ] && [ -f "/etc/redhat-release" ]; then
+        echo
+        echo "To load the $MODULE_NAME module on boot, add the following line in /etc/rc.d/rc.local:"
+        echo "modprobe mods"
+    elif [ -f "/etc/sysconfig/kernel" ] && [ -f "/etc/SuSE-release" ]; then
+        echo
+        echo "To load the $MODULE_NAME module on boot, add it to MODULES_LOADED_ON_BOOT"
+        echo "in /etc/sysconfig/kernel"
+    fi
+fi
+
+# Try to insert the compiled module
+if [ "$OPTION" = "insert" ]; then
+    $INSMOD "${MODULE_DIR}${MODULE_NAME}.ko" || die "Unable to install the module"
+else
+    $MODPROBE "${MODULE_NAME}" || die "Unable to install the module"
+fi
+echo "Loaded module ${MODULE_NAME}"
+
